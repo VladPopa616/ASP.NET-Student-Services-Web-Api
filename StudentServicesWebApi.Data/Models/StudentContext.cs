f2e7199f5ad74dc8a90006b83c4bbe250a13ae71
Vlad Popa <vpopa18@gmail.com>
2021-12-02 12:45:19 -0500

    Set Up Data Layer and added Models.

---
+++ StudentServicesWebApi.Data/Models/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentServicesWebApi.Data.Models
+{
+    public class StudentContext: DbContext
+    {
+        public StudentContext(): base("name=StudentContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
